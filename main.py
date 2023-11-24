e296c92b713bdce339b6f529d92998436de227da
Rezar <97891914+RezarTheSergal@users.noreply.github.com>
2023-11-24 12:12:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from csv import *
 
+# Открываем и читаем файл по стандарту "utf-8"
 with open("/home/itclass/Документы/TestProj/students.csv", encoding="utf-8") as infile:
+    # Парсинг студентов в отдельную переменную построчно
     students_data = reader(infile, delimiter=",")
+
+    # Счётчик
     counter = 0
+
+    # Перебор всех людей в списке и поиск совпадений в ФИО независимо от регистра
     for item in students_data:
-        if "Юля".lower() in item[1].lower():
+        if "Мухы".lower() in item[1].lower():
             counter += 1
             print(item)
     print(counter)
